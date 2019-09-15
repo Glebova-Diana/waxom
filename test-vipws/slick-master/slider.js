a5d482e9b13b91f441e5dd84454c89490a150b85
Glebova Diana <glebova.di5@gmail.com>
2019-09-15 16:49:52 +0300

    add slider adaptive

--- test-vipws/slick-master/slider.js
+++ test-vipws/slick-master/slider.js
@@ -1,8 +1,38 @@
 $(document).ready(function(){
-    $('.slider-wrapper').slick({
-        dots: false,
+    $('.slider').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        centerPadding: '60px',
+        speed: 500,
         slidesToShow: 5,
+        dotsClass: 'slick-dots',
         slidesToScroll: 1,
-        touchMove: false
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    infinite: true,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: true,
+                    dots: false
+                }
+            }
+            ]
     });
 });
