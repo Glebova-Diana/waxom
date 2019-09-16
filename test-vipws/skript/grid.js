8ff84a959fc35fb4c45e4ac1abec814bfceda7bb
Glebova Diana <glebova.di5@gmail.com>
2019-09-16 13:01:09 +0300

    clear code

---
+++ test-vipws/skript/grid.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#main').pinterest_grid({
+        no_columns: 3,
+        padding_x: 10,
+        padding_y: 10,
+        margin_bottom: 50,
+        single_column_breakpoint: 480
+    });
+});
